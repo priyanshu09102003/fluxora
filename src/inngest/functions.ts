13600bb49bdbadba3fffcee95fdb7c876c601a04
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-01-15 11:14:27 +0530

    Integrating all AI Providers to inngest: Google, OpenAI and Anthropic and perform background tasks using them in the inngest client

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,14 +1,21 @@
 import { inngest } from "./client";
 import {createGoogleGenerativeAI} from "@ai-sdk/google"
+import {createOpenAI} from "@ai-sdk/openai"
+import {createAnthropic} from "@ai-sdk/anthropic"
 import {generateText} from "ai"
 
-const google = createGoogleGenerativeAI()
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
+
 
 export const execute = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
+  
   async ({ event, step }) => {
-   const {steps} = await step.ai.wrap(
+
+   const {steps: geminiSteps} = await step.ai.wrap(
     "gemini-generate-text" , 
     generateText, {
     model:google("gemini-2.5-flash"),
@@ -16,6 +23,26 @@ export const execute = inngest.createFunction(
     prompt: "What is 2+2 ?"
    })
 
-   return steps;
+   const {steps: openaiSteps} = await step.ai.wrap(
+    "gemini-generate-text" , 
+    generateText, {
+    model:openai("gpt-4"),
+    system: "You are a helpful assistant.",
+    prompt: "What is 2+2 ?"
+   })
+
+   const {steps: anthropicSteps} = await step.ai.wrap(
+    "gemini-generate-text" , 
+    generateText, {
+    model:anthropic("claude-sonnet-4-5"),
+    system: "You are a helpful assistant.",
+    prompt: "What is 2+2 ?"
+   })
+
+   return{
+    geminiSteps,
+    openaiSteps,
+    anthropicSteps
+   }
   },
 );
