8fa89a5ee28ec745f9781b6bcd8183c0407b14ac
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-01-26 21:33:47 +0530

    Node templating for dynamic json body for the http request and api endpoints

--- src/features/executions/http-request/executor.ts
+++ src/features/executions/http-request/executor.ts
@@ -1,11 +1,20 @@
 import { NonRetriableError } from "inngest";
 import { NodeExecutor } from "../types"
 import ky, {type Options as KyOptions} from "ky"
+import Handlebars from "handlebars"
+
+
+Handlebars.registerHelper("json" , (context) => {
+    const jsonString = JSON.stringify(context, null, 2);
+    const safeString = new Handlebars.SafeString(jsonString)
+
+    return safeString
+});
 
 type HTTPRequestData = {
-    variableName?: string;
-    endpoint?: string;
-    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    variableName: string;
+    endpoint: string;
+    method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string
 }
 
@@ -25,6 +34,12 @@ export const HTTPRequestExecutor: NodeExecutor<HTTPRequestData> = async({
         throw new NonRetriableError("HTTP Request Node: No Endpoint Configured")
     }
 
+    if(!data.method){
+        //Error Statte
+
+        throw new NonRetriableError("Method not Configured")
+    }
+
     if(!data.variableName){
         //Error Statte
 
@@ -34,13 +49,15 @@ export const HTTPRequestExecutor: NodeExecutor<HTTPRequestData> = async({
     
 
     const result = await step.run("http-request", async() => {
-        const endpoint = data.endpoint!;
-        const method = data.method || "GET";
+        const endpoint = Handlebars.compile(data.endpoint)(context);
+        const method = data.method;
 
         const options: KyOptions = {method};
 
         if(["POST" , "PUT", "PATCH"].includes(method)){
-            options.body = data.body;
+            const resolved = Handlebars.compile(data.body || "{}")(context);
+            JSON.parse(resolved)
+            options.body = resolved;
             options.headers = {
                 "Content-Type": "application/jsonf"
             }
@@ -60,20 +77,12 @@ export const HTTPRequestExecutor: NodeExecutor<HTTPRequestData> = async({
 
         }
 
-        if(data.variableName){
 
             return{
                 ...context,
                 [data.variableName]:responsePayload
             }
-        }
-
-
-        //Fallback
-        return {
-            ...context,
-            ...responsePayload
-        }
+        
 
 
     })
