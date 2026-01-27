e95428a4fa8b71f4cd9ebeb34c01fd9b2cd20752
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-01-27 11:17:36 +0530

    Inngest testing and realtime integration for manual triggers

--- src/features/manual-triggers/node.tsx
+++ src/features/manual-triggers/node.tsx
@@ -3,11 +3,24 @@ import { NodeProps } from "@xyflow/react";
 import { MousePointerIcon } from "lucide-react";
 import { memo, useState } from "react";
 import { ManualTriggerDialog } from "./dialog";
+import { useNodeStatus } from "@/hooks/use-node-status";
+import { fetchManualTriggerRealtimeToken } from "./actions";
+import { MANUAL_TRIGGER_CHANNEL_NAME } from "@/inngest/channels/manual-trigger-channel";
+
 
 export const ManualTriggerNode = memo((props: NodeProps) => {
     const [dialogOpen, setDialogOpen] = useState(false)
     const handleOpenSettings = () => setDialogOpen(true);
-    const nodeStatus = "initial"
+
+
+
+        const nodeStatus = useNodeStatus({
+            nodeId: props.id,
+            channel: MANUAL_TRIGGER_CHANNEL_NAME,
+            topic: "status",
+            refreshToken: fetchManualTriggerRealtimeToken
+        });
+        
     return(
         <>
             <ManualTriggerDialog open={dialogOpen} onOpenChange={setDialogOpen} />
