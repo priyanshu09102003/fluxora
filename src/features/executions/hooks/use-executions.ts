c61051b4601f55c7440de95a4e43eca5c0672db3
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-02-04 00:48:46 +0530

    Creating the hooks for the executions

---
+++ src/features/executions/hooks/use-executions.ts
@@ -0,0 +1,20 @@
+
+import { useTRPC } from "@/trpc/client"
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useExecutionsParams } from "./use-executions-params";
+
+
+export const useSuspenseExecutions = () => {
+    const trpc = useTRPC();
+    const [params] = useExecutionsParams()
+
+    return useSuspenseQuery(trpc.executions.getMany.queryOptions(params))
+};
+
+//Hook to fetch a single Execution using suspense
+
+export const useSuspenseExecution = (id: string) => {
+    const trpc = useTRPC();
+    return useSuspenseQuery(trpc.executions.getOne.queryOptions({id}))
+}
+
