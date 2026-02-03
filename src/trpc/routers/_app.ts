277fed9ceca22add5e8fdbf1908ca096c299a2a8
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-02-04 00:34:25 +0530

    Server components and params for executions- routers, prefetch and loader

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,10 +4,12 @@ import {createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 import { workFlowsRouter } from '@/features/workflows/server/routers';
 import { credentialsRouter } from '@/features/credentials/server/routers';
+import { executionssRouter } from '../../features/executions/server/routers';
 
 export const appRouter = createTRPCRouter({
   workflows: workFlowsRouter,
-  credentials: credentialsRouter
+  credentials: credentialsRouter,
+  executions: executionssRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
